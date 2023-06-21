b4be22908f6f8417a61c4b33b004ba9d494d0c57
carloeusebi <carloeusebi@gmail.com>
2023-06-21 16:23:55 +0200

    New messages get proper new id

--- js/scripts.js
+++ js/scripts.js
@@ -31,13 +31,8 @@ const app = Vue.createApp({
         },
 
         activeContact() {
-            return this.contacts.find(({ id }) => id === this.activeContactId);
-        },
-
-        newMessageId() {
-            //todo proper logic
-            return 5;
-        },
+            return this.getActiveContact();
+        }
     },
 
     methods: {
@@ -45,30 +40,43 @@ const app = Vue.createApp({
             this.activeContactId = id;
         },
 
+        getActiveContact() {
+            return this.contacts.find(({ id }) => id === this.activeContactId);
+        },
+
         sendMessage() {
+            /**
+             * calculates the current time
+             * @returns the current times
+             */
             const getCurrentTime = () => {
                 const dateObj = new Date()
                 const date = dateObj.toLocaleDateString();
                 const time = dateObj.toLocaleTimeString();
 
                 return date + ' ' + time;
-
             };
 
+            /**
+             * Checks all the contact messages ids and returns the lowest possibile non present id
+             * @param {object} contact 
+             * @returns 
+             */
+            const getMessageId = contact => activeContact.messages.reduce((highest, { id }) => id > highest ? id : highest, 0) + 1;
+
+            const activeContact = this.getActiveContact();
+
             const message = this.newMessage;
-            const id = this.newMessageId;
+
+            if (!message) return;
+
+            const id = getMessageId();
             const date = getCurrentTime();
             const status = 'sent';
 
             const justSentMessage = { id, date, message, status }
 
-            this.data.contacts.forEach(contact => {
-                if (contact.id === this.activeContactId) {
-                    contact.messages.push(justSentMessage);
-                }
-            })
-
-
+            activeContact.messages.push(justSentMessage);
 
             this.newMessage = '';
         }
